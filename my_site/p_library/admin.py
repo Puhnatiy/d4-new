083cbb67642c22f89808c438c7d75b022820cf8f
Puhnatiy <55509995+Puhnatiy@users.noreply.github.com>
2020-08-07 21:20:37 +0300

    Add files via upload

---
+++ my_site/p_library/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from p_library.models import Book, Author, Publisher
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    @staticmethod
+    def author_full_name(obj):
+        return obj.author.full_name
+    
+    list_display = ('title', 'author')
+    fields = ('ISBN', 'title', 'description', 'year_release', 'author', 'price', 'publisher')
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+    
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    pass

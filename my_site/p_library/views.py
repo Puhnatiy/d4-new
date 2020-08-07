664936b5c9344090fdc780cc1812511b5a269a7c
Puhnatiy <55509995+Puhnatiy@users.noreply.github.com>
2020-08-07 21:21:45 +0300

    Add files via upload

---
+++ my_site/p_library/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from p_library.models import Book
+from p_library.models import Publisher
+from django.http import HttpResponse
+from django.template import loader
+
+def books_list(request):
+    books = Book.objects.all()
+    return HttpResponse(books)
+
+def index(request):
+    template = loader.get_template('index.html')
+    books = Book.objects.all()
+    list_100 = range(1,101)
+    biblio_data = {
+        "title": "мою библиотеку",
+        "books": books,
+        "list_100": list_100,
+    }
+    return HttpResponse(template.render(biblio_data, request))
+    
+def book_increment(request):
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if not book_id:
+            return redirect('/index/')
+        else:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index/')
+            book.copy_count += 1
+            book.save()
+        return redirect('/index/')
+    else:
+        return redirect('/index/')
+
+
+def book_decrement(request):
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if not book_id:
+            return redirect('/index/')
+        else:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index/')
+            if book.copy_count < 1:
+                book.copy_count = 0
+            else:
+                book.copy_count -= 1
+            book.save()
+        return redirect('/index/')
+    else:
+        return redirect('/index/')
+
+def publisher_list(request):
+    template = loader.get_template('publishers.html')
+    pubs = Publisher.objects.all()
+    biblio_data = {      
+        "pubs": pubs,
+    }
+    return HttpResponse(template.render(biblio_data, request))
